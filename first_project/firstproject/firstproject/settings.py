b00943ce9b5351964adaea6278108f4bd539c5d1
Anand Gowda <happyanand849@gmail.com>
2021-07-03 21:27:58 +0530

    secret key is hidden and added requirements file

--- first_project/firstproject/firstproject/settings.py
+++ first_project/firstproject/firstproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 import os
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--2ygp^2v0hj4yxh@muu@k9v+s1_c)==d%0$^is62x@%n-!e+4k'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
